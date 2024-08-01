7ae69bae8c4658e51eeeff050a654d8698c782c0
Ashwin Chandar <rashwinchandar@gmail.com>
2024-08-01 15:07:42 +0530

    Changed the URL to AWS

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000',
+  // baseURL: 'http://localhost:4000',
+  baseURL: 'https://bbxaulogg4cfrdzpheaislkkfe0wafen.lambda-url.us-east-2.on.aws'
 });
 
 API.interceptors.request.use(
