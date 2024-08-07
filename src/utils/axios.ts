45eba68dae5a5c4e431fc1ad58cfc2fc4332a478
Ashwin Chandar <rashwinchandar@gmail.com>
2024-08-07 23:51:03 +0530

    Second deployment 1

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -33,6 +33,7 @@ export const fetcher = async (args: string | [string, AxiosRequestConfig]) => {
 // ----------------------------------------------------------------------
 
 // const backendUrl = 'http://localhost:4000';
+
 const backendUrl = 'https://bbxaulogg4cfrdzpheaislkkfe0wafen.lambda-url.us-east-2.on.aws';
 
 export const endpoints = {
